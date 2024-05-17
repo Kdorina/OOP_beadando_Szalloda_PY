fe694fafc5e2fea7759f126d46d552db537cc072
Kdorina <kovacsdorina2@gmail.com>
2024-05-18 00:11:32 +0200

    felhasználói interfesz figgveny letrehozása

--- oop_beadando.py
+++ oop_beadando.py
@@ -79,6 +79,32 @@ def foglalas_listazas():
         foglalas_lista.append(f"Szoba: {f.szoba_szam}, Dátum: {f.datum}")
     return foglalas_lista
 
+def felhasznaloi_interfesz():
+    while True:
+        print("1. Foglalás létrehozása")
+        print("2. Foglalás lemondása")
+        print("3. Foglalások listázása")
+        print("4. Kilépés")
+        valasztas = input("Kérem válasszon egy műveletet: ")
+
+        if valasztas == "1":
+            szoba_szam = int(input("Szoba száma: "))
+            datum = input("Dátum (YYYY-MM-DD): ")
+            datum = datetime.strptime(datum, "%Y-%m-%d").date()
+            print(szoba_fogalalas(szalloda, szoba_szam, datum))
+        elif valasztas == "2":
+            szoba_szam = int(input("Szoba száma: "))
+            datum = input("Dátum (YYYY-MM-DD): ")
+            datum = datetime.strptime(datum, "%Y-%m-%d").date()
+            print(foglalas_lemondas(szoba_szam, datum))
+        elif valasztas == "3":
+            for foglalas in foglalas_listazas():
+                print(foglalas)
+        elif valasztas == "4":
+            break
+        else:
+            print("Érvénytelen választás, kérem próbálja újra.")
+
 szalloda = Szalloda("Pandora Grand Hotel")
 szalloda.szoba_felvetel(EgyagyasSzoba(101))
 szalloda.szoba_felvetel(KetagyasSzoba(102))
