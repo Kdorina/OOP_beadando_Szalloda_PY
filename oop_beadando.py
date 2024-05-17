96a4a0b5be98feb8231cf55b807b2af51a8d9ebe
Kdorina <kovacsdorina2@gmail.com>
2024-05-18 01:22:03 +0200

    modositas

--- oop_beadando.py
+++ oop_beadando.py
@@ -9,19 +9,22 @@ class Szoba(ABC):
     @abstractmethod
     def szoba_tipus(self):
         pass
+
 class EgyagyasSzoba(Szoba):
     def __init__(self, szam):
         super().__init__(szam, 10000)
+        self.tipus = "Egyágyas szoba"
 
     def szoba_tipus(self):
-        return "Egyágyas szoba"
+        return self.tipus
 
 class KetagyasSzoba(Szoba):
     def __init__(self, szam):
         super().__init__(szam, 15000)
+        self.tipus = "Kétágyas szoba"
 
     def szoba_tipus(self):
-        return "Kétágyas szoba"
+        return self.tipus
 
 class Szalloda:
     def __init__(self, nev):
@@ -32,10 +35,9 @@ class Szalloda:
         if szoba not in self.szobak:
             self.szobak.append(szoba)
         else:
-            print(f"A {szoba.szam} számú szoba már létezik a szállodában.")
+            print(f"\n\nA {szoba.szam} számú szoba már létezik a szállodában.\n")
 
     def elerheto_szobak(self, datum):
-        
         foglalt_szobak = set()
         for f in foglalasok:
             if f.datum == datum:
@@ -49,34 +51,35 @@ class Szalloda:
         return elerheto_szobak
     
 class Foglalas:
-    def __init__(self, szoba_szam, datum):
+    def __init__(self, szoba_szam, datum, szoba_tipus):
         self.szoba_szam = szoba_szam
         self.datum = datum
+        self.szoba_tipus = szoba_tipus
 
 foglalasok = []
 
 def szoba_fogalalas(szalloda, szoba_szam, datum):
     if datum <= date.today():
-        return 'Kérem adjon meg jövőbeni dátumot!'
+        return '\n\nKérem adjon meg jövőbeni dátumot!\n'
     elerheto_szobak = szalloda.elerheto_szobak(datum)
     for szoba in elerheto_szobak:
-        if szoba_szam == szoba_szam:
-            uj_foglalas = Foglalas(szoba_szam, datum)
+        if szoba_szam == szoba.szam:
+            uj_foglalas = Foglalas(szoba_szam, datum, szoba.szoba_tipus())
             foglalasok.append(uj_foglalas)
-            return f"Gratulálunk, foglalása sikeres volt! Foglalásának összege: {szoba.ar} Ft"
-    return "Sajnálattal közöljük, a szoba nem elérhető a kiválaszott dátumon"
+            return f"\n\nGratulálunk, foglalása sikeres volt! Foglalásának összege: {szoba.ar} Ft, Szoba típus: {szoba.szoba_tipus()}\n"
+    return "\n\nSajnálattal közöljük, a szoba nem elérhető a kiválaszott dátumon\n"
 
 def foglalas_lemondas(szoba_szam, datum):
     for foglalas in foglalasok:
         if foglalas.szoba_szam == szoba_szam and foglalas.datum == datum:
             foglalasok.remove(foglalas)
-            return "Fogalalását sikeresen lemondta!"
-    return "Nincs ilyen foglalás!"
+            return "\n\nFoglalását sikeresen lemondta!"
+    return "\n\nNincs ilyen foglalás!"
 
 def foglalas_listazas():
     foglalas_lista = []
     for f in foglalasok:
-        foglalas_lista.append(f"Szoba: {f.szoba_szam}, Dátum: {f.datum}")
+        foglalas_lista.append(f"Szoba: {f.szoba_szam}, Dátum: {f.datum}, Szoba típus: {f.szoba_tipus}")
     return foglalas_lista
 
 def felhasznaloi_interfesz():
@@ -103,17 +106,17 @@ def felhasznaloi_interfesz():
         elif valasztas == "4":
             break
         else:
-            print("Érvénytelen választás, kérem próbálja újra.")
+            print("\n\nÉrvénytelen választás, kérem próbálja újra.\n")
 
 szalloda = Szalloda("Pandora Grand Hotel")
 szalloda.szoba_felvetel(EgyagyasSzoba(101))
 szalloda.szoba_felvetel(KetagyasSzoba(102))
 szalloda.szoba_felvetel(KetagyasSzoba(103))
 
-foglalasok.append(Foglalas(101, date(2024, 5, 20)))
-foglalasok.append(Foglalas(101, date(2024, 5, 21)))
-foglalasok.append(Foglalas(102, date(2024, 5, 20)))
-foglalasok.append(Foglalas(102, date(2024, 5, 21)))
-foglalasok.append(Foglalas(103, date(2024, 5, 22)))
+foglalasok.append(Foglalas(101, date(2024, 5, 20), "Egyágyas szoba"))
+foglalasok.append(Foglalas(101, date(2024, 5, 21), "Egyágyas szoba"))
+foglalasok.append(Foglalas(102, date(2024, 5, 20), "Kétágyas szoba"))
+foglalasok.append(Foglalas(102, date(2024, 5, 21), "Kétágyas szoba"))
+foglalasok.append(Foglalas(103, date(2024, 5, 22), "Kétágyas szoba"))
 
 felhasznaloi_interfesz()
